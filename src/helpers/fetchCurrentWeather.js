645c24b3651d8ac62336ac725ac7ce9fcf64ce68
jmm <josemmazzei@gmail.com>
2020-06-19 09:09:32 -0300

    Refactor fetchCurrentWeather
    Just improve legibility by destructuring the props in the function
    signature.

--- src/helpers/fetchCurrentWeather.js
+++ src/helpers/fetchCurrentWeather.js
@@ -1,5 +1,5 @@
-export default async coords => {
-    return await fetch(
-        `https://api.openweathermap.org/data/2.5/weather?lat=${coords.latitude}&lon=${coords.longitude}&units=metric&APPID=${process.env.REACT_APP_API_KEY}`
-    ).then(res => res.json())
+export default async ({latitude,longitude}) => {
+  return await fetch(
+    `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=metric&APPID=${process.env.REACT_APP_API_KEY}`,
+  ).then((res) => res.json())
 }
