386895e214831d5c6885a73516bb8ff7f4d5ebf4
José María <josemmazzei@gmail.com>
2022-04-09 13:13:58 -0300

    Refactor Footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,29 +6,29 @@ type Props = {
 }
 
 const Footer = ({ uniqueDate, click }: Props) => {
+  if (!uniqueDate) {
+    return (
+      <footer className="footer">
+        {Array(5)
+          .fill(null)
+          .map((el) => (
+            <FooterCard />
+          ))}
+      </footer>
+    )
+  }
+
   return (
     <footer className="footer">
-      {uniqueDate ? (
-        uniqueDate.map(
-          (e) =>
-            e[1].length !== 0 && (
-              <FooterCard
-                date={e[0]}
-                click={() => click(e[1])}
-                key={e[1][0].dt}
-              />
-            ),
-        )
-      ) : (
-        <>
-          {(() => {
-            let footerCardArr = []
-            for (let i = 0; i < 5; i++) {
-              footerCardArr.push(<FooterCard />)
-            }
-            return footerCardArr
-          })()}
-        </>
+      {uniqueDate.map(
+        (e) =>
+          e[1].length !== 0 && (
+            <FooterCard
+              date={e[0]}
+              click={() => click(e[1])}
+              key={e[1][0].dt}
+            />
+          ),
       )}
     </footer>
   )
